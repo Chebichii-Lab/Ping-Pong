0920e1ef0b5fbf429495421c1154e19b41ee750e
Chebichii-Lab <chebichiiserem@gmail.com>
2023-12-05 12:40:52 +0300

    add button movements

--- pong_but_better.py
+++ pong_but_better.py
@@ -26,7 +26,7 @@ ball_vel_x, ball_vel_y = 0.7, 0.7
 paddle_width, paddle_height = 20, 120
 left_paddle_y = right_paddle_y = HEIGHT/2 -paddle_height/2
 left_paddle_x, right_paddle_x = 100 - paddle_width/2, WIDTH -(100 - paddle_width/2)
-
+right_paddle_vel = left_paddle_vel = 0
 
 
 #mainloop
@@ -35,6 +35,19 @@ while run:
     for i in pygame.event.get(): 
         if i.type == pygame.QUIT:
             run = False
+        elif i.type == pygame.KEYDOWN:
+            #if key pressed is up or down arrow then move the paddles accordingly
+            if i.key == pygame.K_UP:
+                right_paddle_vel = -0.9
+            if i.key == pygame.K_DOWN:
+                right_paddle_vel = 0.9
+            if i.key == pygame.K_w:
+                left_paddle_vel = -0.9
+            if i.key == pygame.K_s:
+                left_paddle_vel = 0.9
+        if i.type == pygame.KEYUP:
+            right_paddle_vel = 0
+            left_paddle_vel = 0
 
     #ball's movement controls
     if ball_y <= 0 + radius or ball_y >= HEIGHT -radius:
@@ -54,6 +67,8 @@ while run:
     #movements
     ball_x += ball_vel_x
     ball_y += ball_vel_y
+    right_paddle_y += right_paddle_vel
+    left_paddle_y += left_paddle_vel
 
 
 #drawing of objects
