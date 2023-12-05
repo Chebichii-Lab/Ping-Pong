3dba94122b072730c9aa8fe68bc4abc5468a6ee4
Chebichii-Lab <chebichiiserem@gmail.com>
2023-12-05 11:47:43 +0300

    creating the objects for the game; the ball measurements

--- pong_but_better.py
+++ pong_but_better.py
@@ -9,8 +9,19 @@ wn = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Nat's Ping Pong")
 run = True
 
+#colors
+BLUE = (0, 0, 255)
+
+#for the ball
+# keeping the ball at the exact center of the screen
+radius = 15
+ball_x, ball_y = WIDTH/2 - radius, HEIGHT/2 - radius
+
 #mainloop
 while run:
     for i in pygame.event.get(): 
         if i.type == pygame.QUIT:
             run = False
+
+#drawing of objects
+    pygame.draw.circle(wn. BLUE, (ball_x, ball_y), radius )
